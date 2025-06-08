bccbc52c14973dfc87bc25e7fd2a6ea52c3806d0
DesertChaos <vadimsysolin52565@gmail.com>
2025-06-09 01:12:05 +0300

    Add files via upload

---
+++ cars/cars/Avto.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cars
+{
+
+    public class Avto
+    {
+        public string Name { get; set; }
+        public int TankCapacity { get; set; }
+        public double FuelLevel { get; set; }
+        public double FuelConsumption { get; set; }
+        public double Mileage { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public int Speed { get; set; }
+        public int MaxSpeed { get; set; }
+
+        public Avto(string name, int tankCapacity, double fuelLevel, double fuelConsumption, double mileage, double x, double y, int maxSpeed)
+        {
+            Name = name;
+            TankCapacity = tankCapacity;
+            FuelLevel = fuelLevel;
+            FuelConsumption = fuelConsumption;
+            Mileage = mileage;
+            X = x;
+            Y = y;
+            MaxSpeed = maxSpeed;
+            Speed = 0;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Номер машины: {Name}");
+            Console.WriteLine($"Кординаты: x:{X} y:{Y}");
+            Console.WriteLine($"Скорость: {Speed}");
+            Console.WriteLine($"Расход топлива: {FuelConsumption}");
+            Console.WriteLine($"Топливо: {FuelLevel}/{TankCapacity}");
+            Console.WriteLine($"Пробег: {Mileage}");
+        }
+
+        public void MoveTo(double newX, double newY)
+        {
+            if (Speed == 0)
+            {
+                Console.WriteLine("Машина не движется. Нельзя переместиться.");
+                return;
+            }
+
+            double distance = Math.Sqrt(Math.Pow(newX - X, 2) + Math.Pow(newY - Y, 2));
+            double fuelConsumed = (distance / 100) * FuelConsumption;
+
+            if (FuelLevel >= fuelConsumed)
+            {
+                Console.WriteLine($"Перемещение на расстояние {distance} км потребует {fuelConsumed} литров топлива.");
+                Console.Write("Продолжить? (да/нет): ");
+                string answer = Console.ReadLine();
+
+                if (answer.ToLower() == "да")
+                {
+                    FuelLevel -= fuelConsumed;
+                    X = newX;
+                    Y = newY;
+                    Mileage += distance;
+                    Console.WriteLine($"Машина переместилась в координаты x:{X} y:{Y}");
+                }
+                else
+                {
+                    Console.WriteLine("Перемещение отменено.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Недостаточно топлива. (Требуется {fuelConsumed}/ Осталось {FuelLevel})");
+            }
+        }
+
+        public void Refuel()
+        {
+            Console.WriteLine($"Текущий уровень топлива: {FuelLevel}/{TankCapacity}");
+            Console.Write("На сколько дозаправить: ");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out double amount))
+            {
+                if (amount <= 0)
+                {
+                    Console.WriteLine("Некорректный ввод. (Недопустимое количество топлива)");
+                }
+                else if (FuelLevel + amount > TankCapacity)
+                {
+                    Console.WriteLine("Некорректный ввод. (Бак переполнится)");
+                }
+                else
+                {
+                    FuelLevel += amount;
+                    Console.WriteLine($"Машина заправлена. Текущий уровень топлива: {FuelLevel}/{TankCapacity}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+        }
+
+        public void Accelerate()
+        {
+            if (Speed < MaxSpeed)
+            {
+                Speed += 10;
+                Console.WriteLine($"Скорость увеличена до {Speed} км/ч");
+            }
+            else
+            {
+                Console.WriteLine("Максимальная скорость достигнута.");
+            }
+        }
+
+        public void Brake()
+        {
+            if (Speed > 0)
+            {
+                Speed -= 10;
+                if (Speed < 0) Speed = 0;
+                Console.WriteLine($"Скорость уменьшена до {Speed} км/ч");
+            }
+            else
+            {
+                Console.WriteLine("Машина уже стоит на месте.");
+            }
+        }
+
+        public static void CheckCrash(Avto avto, List<Avto> avtos)
+        {
+            foreach (var otherAvto in avtos)
+            {
+                if (otherAvto != avto && otherAvto.X == avto.X && otherAvto.Y == avto.Y)
+                {
+                    Console.WriteLine($"Авария! Машина {avto.Name} столкнулась с машиной {otherAvto.Name}.");
+                    Console.WriteLine("Обе машины были перемещены в координаты x: 0 y: 0");
+                    avto.X = 0;
+                    avto.Y = 0;
+                    otherAvto.X = 0;
+                    otherAvto.Y = 0;
+                }
+            }
+        }
+    }
+}
---
+++ cars/cars/Program.cs
@@ -0,0 +1,159 @@
+﻿using System.Xml.Linq;
+
+namespace cars
+{
+    class Program
+    {
+        static List<Avto> avtos = new List<Avto>();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Меню:");
+                Console.WriteLine("1. Создать новую машину");
+                Console.WriteLine("2. Выбрать машину");
+                Console.WriteLine("3. Выйти");
+
+                Console.Write("Введите номер действия: ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        CreateAvto();
+                        break;
+                    case "2":
+                        ChooseAvto();
+                        break;
+                    case "3":
+                        return;
+                    default:
+                        Console.WriteLine("Некорректный ввод.");
+                        break;
+                }
+            }
+        }
+
+        static void CreateAvto()
+        {
+            Console.Write("Введите имя машины: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите емкость бака: ");
+            int tankCapacity = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите расход топлива: ");
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите максимальную скорость: ");
+            int maxSpeed = int.Parse(Console.ReadLine());
+
+            Avto avto = new Avto(name, tankCapacity, 0, fuelConsumption, 0, 0, 0, maxSpeed);
+            avtos.Add(avto);
+
+            Console.WriteLine("Машина создана.");
+        }
+
+        static void ChooseAvto()
+        {
+            if (avtos.Count == 0)
+            {
+                Console.WriteLine("Нет машин.");
+                return;
+            }
+
+            Console.WriteLine("Список машин:");
+            for (int i = 0; i < avtos.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {avtos[i].Name}");
+            }
+
+            Console.Write("Введите номер машины: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int index) && index > 0 && index <= avtos.Count)
+            {
+                Avto avto = avtos[index - 1];
+
+                Console.Clear();
+                
+                Console.WriteLine($"Вы в машине {avto.Name}");
+                Console.WriteLine($"Кординаты: x:{avto.X} y:{avto.Y}");
+                Console.WriteLine($"Скорость: {avto.Speed}");
+                Console.WriteLine($"Топливо: {avto.FuelLevel}/{avto.TankCapacity}");
+
+                while (true)
+                {
+                    Console.WriteLine("Меню машины:");
+                    Console.WriteLine("1. Вывести информацию");
+                    Console.WriteLine("2. Переместить");
+                    Console.WriteLine("3. Дозаправить");
+                    Console.WriteLine("4. Разогнать");
+                    Console.WriteLine("5. Тормозить");
+                    Console.WriteLine("6. Назад");
+
+                    Console.Write("Введите номер действия: ");
+                    string action = Console.ReadLine();
+
+                    switch (action)
+                    {
+                        case "1":
+                            Console.Clear();
+                            avto.PrintInfo();
+                            break;
+                        case "2":
+                            Console.Clear();
+                            Console.Write("Введите координату X, в которую вы хотите переместить машину: ");
+                            string xInput = Console.ReadLine();
+                            if (double.TryParse(xInput, out double newX))
+                            {
+                                Console.Write("Введите координату Y, в которую вы хотите переместить машину: ");
+                                string yInput = Console.ReadLine();
+                                if (double.TryParse(yInput, out double newY))
+                                {
+                                    avto.MoveTo(newX, newY);
+                                    Avto.CheckCrash(avto, avtos);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Некорректный ввод координаты Y.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Некорректный ввод координаты X.");
+                            }
+                            break;
+                        case "3":
+                            Console.Clear();
+                            avto.Refuel();
+                            break;
+                        case "4":
+                            Console.Clear();
+                            avto.Accelerate();
+                            break;
+                        case "5":
+                            Console.Clear();
+                            avto.Brake();
+                            break;
+                        case "6":
+                            Console.Clear();
+                            return;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Некорректный ввод.");
+                            break;
+                    }
+                    Console.WriteLine("Нажмите любую клавишу для продолжения...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+        }
+    }
+}
